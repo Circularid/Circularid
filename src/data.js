0012fb4c31bb399e79ed8e622791e0c0787789d9
Eloy Larico <eloylarico@gmail.com>
2023-07-03 12:36:42 -0500

    subiendo plugin circularId

---
+++ src/data.js
@@ -0,0 +1,28 @@
+
+const data = [
+  {
+    image: 'https://magento-circular.bgroup.com.ar/media/catalog/product/p/u/puede_servir_para_ambascc.png',
+  },
+  {
+    image: 'https://magento-circular.bgroup.com.ar/media/catalog/product/p/u/puede_servir_para_ambasc.png',
+  },
+  {
+    image: 'https://magento-circular.bgroup.com.ar/media/catalog/product/p/u/puede_servir_para_ambascapa_3.png',
+  },
+  {
+    image: 'https://magento-circular.bgroup.com.ar/media/catalog/product/p/u/puede_servir_para_ambasccc.png',
+  },
+  {
+    image: 'https://magento-circular.bgroup.com.ar/media/catalog/product/p/u/puede_servir_para_ambascc.png',
+  },
+  {
+    image: 'https://magento-circular.bgroup.com.ar/media/catalog/product/p/u/puede_servir_para_ambasc.png',
+  },
+  {
+    image: 'https://magento-circular.bgroup.com.ar/media/catalog/product/p/u/puede_servir_para_ambascapa_3.png',
+  },
+  {
+    image: 'https://magento-circular.bgroup.com.ar/media/catalog/product/p/u/puede_servir_para_ambasccc.png',
+  },
+];
+export default data;
